8d6db90177dfac8a99973fc53a0aa7aaf2c9e961
kingola1 <segun8427@gmail.com>
2024-12-30 19:52:11 +0100

    Made verificattiio match the new

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout isAuth="true">
     <section class="flex flex-col w-full items-center bg-[url('/images/lines.png')] bg-cover bg-center mb-10 lg:mb-16">
         <div
-            class="bg-gradient-to-r from-[#FFE5C680] to-[#FFBB6780] p-10 md:p-16 lg:p-20 lg:w-[40%] rounded-3xl items-center text-center mt-10 mx-5 md:mx-0 md:mt-16 lg:mt-24">
+            class="bg-gradient-to-r from-[#E6E6F366] to-[#6666B366] p-10 md:p-16 lg:p-20 lg:w-[40%] rounded-3xl items-center text-center mt-10 mx-5 md:mx-0 md:mt-16 lg:mt-24">
             <h1 class="text-3xl md:text-4xl lg:text-5xl font-bold text-black mb-5">Verify Your Email</h1>
             <p class="text-xl mb-8">Thanks for signing up! Before getting started, could you verify your email address by
                 clicking on the link we just emailed to you?</p>
@@ -15,8 +15,7 @@
             <div class="flex flex-col space-y-4">
                 <form method="POST" action="{{ route('verification.send') }}">
                     @csrf
-                    <button type="submit"
-                        class="w-full bg-[#FF8C00] text-white px-5 py-3 rounded-xl hover:bg-[#e67e00]">
+                    <button type="submit" class="w-full bg-[#000080] text-white px-5 py-3 rounded-xl">
                         Resend Verification Email
                     </button>
                 </form>
